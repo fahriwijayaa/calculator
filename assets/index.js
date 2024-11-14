07332ddec163af505ce70892e53ba55efe7d015e
fahriwijayaa <fahriw44@gmail.com>
2024-11-14 14:07:55 +0700

    first commit

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,22 @@
-let input = document.getElementById('screen');
+let input = document.getElementById("screen");
 
 const deleteData = () => {
-    const box = document.querySelector('#screen').value.slice(0, -1);
-    document.querySelector('#screen').value = box;
-}
+  const box = document.querySelector("#screen").value.slice(0, -1);
+  document.querySelector("#screen").value = box;
+};
 
 const add = (i) => {
-    input.value += i;
-}
+  input.value += i;
+};
 
 const allClear = () => {
-    input.value = "";
-}
+  input.value = "";
+};
 
 const equal = () => {
-    if (input.value == undefined || input.value == 0){
-        alert('Tidak Ada Input Nilai');
-    } else {
-        input.value = eval(input.value);
-    }
-}
+  if (input.value == undefined || input.value == 0) {
+    alert("Tidak Ada Input Nilai");
+  } else {
+    input.value = eval(input.value);
+  }
+};
